40dbf48e1be150e9de734aa35ec2c0509ea73a64
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-09-15 11:47:46 -0500

    hello world

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class MyComponent extends Component {
+  render() {
+    return (<h1 className= 'hello'>
+    Hello world
+    </h1>
+  )
+  }
+};
+
+export default MyComponent;
