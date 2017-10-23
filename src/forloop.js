965929985934566937068489517cbb77d502db6f
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-10-23 16:28:27 -0500

    for loop

--- src/forloop.js
+++ src/forloop.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 var indents = [];
 for (var i = 0; i < this.props.level; i++) {
   indents.push(<span className='indent' key={i}></span>);
